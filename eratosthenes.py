13368e5e66fe165eef7880244b647d2acf7c2150
Tim Theede <pez2001@voyagerproject.de>
2012-09-16 15:03:16 +0200

    eratosthenes + prng

---
+++ eratosthenes.py
@@ -0,0 +1,13 @@
+def e():
+	o = 1500000
+	n = o*o
+	tossed = [False]*(o+1)
+
+	p = 2
+	while p*p < n:
+		if(tossed[p] == False):
+			print(p,", ")
+			for j in range(p,o,p):
+				tossed[j] = True
+		p+= 1
+e()
