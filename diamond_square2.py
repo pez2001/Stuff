e3f233e17ee65a6825527e2d3153c55bebf7dd54
Tim Theede <pez2001@voyagerproject.de>
2012-09-23 19:21:22 +0200

    erosion tests

--- diamond_square2.py
+++ diamond_square2.py
@@ -194,7 +194,7 @@ def generate_map(height):
 	#print("==start configuration==")
 	#print(floatmap)
 	#print("==start configuration==")
-	generate_detail(0,height/4,floatmap)
+	generate_detail(0,height,floatmap)
 	#print("==end configuration==")
 	#print(floatmap)
 	#print("==end configuration==")
@@ -216,45 +216,125 @@ def updatefloatmap(floatmap,newfloatmap,unchanged_value):
 	for x in range(pow(2,iterations)):
 		for y in range(pow(2,iterations)):
 			if(newfloatmap[x][y] != unchanged_value):
-				floatmap[x][y] = newfloatmap[x][y]
+				#floatmap[x][y] = newfloatmap[x][y]
+				floatmap[x][y] = (floatmap[x][y]*3 + newfloatmap[x][y]*1.0)/4.0
 				
 
-def erode(x,y,floatmap,newfloatmap):
+				
+"""Regolith and baserock layers, the looseness of sediment, contributing area, slope angle, and variable rainfall patterns. """
+"""
+
+float contributingArea (CELL *cell)
+{
+cell->water = rainfall + contributingArea (all upstream neigbor cells);
+
+return cell->water;
+}
+
+
+Generally, water will carry sediment downstream until it can''t go any further (i.e. it will deposit in a lake, ocean, crater, etc.). 
+"""				
+def FindDeepestNeighbour(x,y,floatmap):
+	positions = [ (-1,-1) , (-1,0) , (-1,1) , (0,-1) , (0,1) , (1,-1) , (1,0) , (1,1) ]
+	random.shuffle(positions)
+	h = pos2height(x,y,floatmap)
+	deepest_h = 0.0
+	deepest_pos = (0,0)
+	for i in positions:
+		if(floatmap[x+i[0]][y+i[1]] < h and (h-floatmap[x+i[0]][y+i[1]]) > deepest_h):
+			deepest_h = h-floatmap[x+i[0]][y+i[1]]	
+			deepest_pos = i
+	return(deepest_pos)
+
+"""
+ht=height at current location 
+lowh=height of lowest neighbor 
+new_lowh = (ht + (1.0-power)*(lowh-ht)) 
+new_ht = (ht + power*(lowh-ht))
+
+"""
+def erode(x,y,scale,amount,floatmap):
 	global iterations
 	max = pow(2,iterations)
 	if( (x<0) or (y<0) or (y>(max-2)) or (x>(max-2))):
 		return
-	if(newfloatmap[x][y] != -1.0):
-		return
-	h_mm =pos2height(x,y,floatmap)
+	#if(newfloatmap[x][y] != -1.0):
+	#	return
+	power = 0.2
+	n = FindDeepestNeighbour(x,y,floatmap)
+	if(n != (0,0)):
+		#print("found neighbour at:",n)
+		h_old = pos2height(x,y,floatmap)
+		h_new = pos2height(x+n[0],y+n[1],floatmap)
+		d = random.uniform(0.0,scale * amount)
+		if(random.choice((0,1)) == 0):
+			a = amount * 1.1
+		else:
+			a = amount * 0.9
+		power = a
+		if(power < 0.5):
+			power = 0.5
+		
+		if(a > 0.0):
+			erode(x+n[0],y+n[1],scale * 0.9,a,floatmap)
+		
+		nnh = (h_old + (1.0-power)*(h_new-h_old)) 
+		nh = (h_old + power*(h_new-h_old))
+		if(nnh>=0.0):
+			setheight(x+n[0],y+n[1],nnh,floatmap)
+		if(nh>=0.0):
+			setheight(x,y,nh,floatmap)
+		#h = h_old - d 
+		#if(h>=0.0):
+		#	setheight(x,y,h,floatmap)
+	else:
+		h_old = pos2height(x,y,floatmap)
+		d = random.uniform(0.0,scale * amount)
+		#h = h_old + d 
+		h = h_old + amount*scale
+		if(h<=1.0):
+			setheight(x,y,h,floatmap)
+		
+		
+	"""h_mm = pos2height(x,y,floatmap)
+	d = random.uniform(0.0,scale)
+	s = scale * 1.01
 	if(h_mm-0.1>=0.0):
-		setheight(x,y,h_mm-random.uniform(0.0,0.1),newfloatmap)
-
+		setheight(x,y,h_mm-d,newfloatmap)
+	
 	h_tl =pos2height(x-1,y-1,floatmap)
-	if(h_tl<h_mm and random.choice((0,1)) == 0):
-		erode(x-1,y-1,floatmap,newfloatmap)
+	#if(h_tl<h_mm and random.choice((0,1,1,1)) == 0):
+	if(h_tl<h_mm):
+		erode(x-1,y-1,s,floatmap,newfloatmap)
 	h_tm =pos2height(x,y-1,floatmap)
-	if(h_tm<h_mm and random.choice((0,1)) == 0):
-		erode(x,y-1,floatmap,newfloatmap)
+	#if(h_tm<h_mm and random.choice((0,1,1,1)) == 0):
+	if(h_tm<h_mm):
+		erode(x,y-1,s,floatmap,newfloatmap)
 	h_tr =pos2height(x+1,y-1,floatmap)
-	if(h_tr<h_mm and random.choice((0,1)) == 0):
-		erode(x+1,y-1,floatmap,newfloatmap)
+	#if(h_tr<h_mm and random.choice((0,1,1,1)) == 0):
+	if(h_tr<h_mm):
+		erode(x+1,y-1,s,floatmap,newfloatmap)
 	h_ml =pos2height(x-1,y,floatmap)
-	if(h_ml<h_mm and random.choice((0,1)) == 0):
-		erode(x-1,y,floatmap,newfloatmap)
+	#if(h_ml<h_mm and random.choice((0,1,1,1)) == 0):
+	if(h_ml<h_mm):
+		erode(x-1,y,s,floatmap,newfloatmap)
 	h_mr =pos2height(x+1,y,floatmap)
-	if(h_mr<h_mm and random.choice((0,1)) == 0):
-		erode(x+1,y,floatmap,newfloatmap)
+	#if(h_mr<h_mm and random.choice((0,1,1,1)) == 0):
+	if(h_mr<h_mm):
+		erode(x+1,y,s,floatmap,newfloatmap)
 	h_bl =pos2height(x-1,y+1,floatmap)
-	if(h_bl<h_mm and random.choice((0,1)) == 0):
-		erode(x-1,y+1,floatmap,newfloatmap)
+	#if(h_bl<h_mm and random.choice((0,1,1,1)) == 0):
+	if(h_bl<h_mm):
+		erode(x-1,y+1,s,floatmap,newfloatmap)
 	h_bm =pos2height(x,y+1,floatmap)
-	if(h_bm<h_mm and random.choice((0,1)) == 0):
-		erode(x,y+1,floatmap,newfloatmap)
+	#if(h_bm<h_mm and random.choice((0,1,1,1)) == 0):
+	if(h_bm<h_mm):
+		erode(x,y+1,s,floatmap,newfloatmap)
 	h_br =pos2height(x+1,y+1,floatmap)
-	if(h_br<h_mm and random.choice((0,1)) == 0):
-		erode(x+1,y+1,floatmap,newfloatmap)
-
+	#if(h_br<h_mm and random.choice((0,1,1,1)) == 0):
+	if(h_br<h_mm):
+		erode(x+1,y+1,s,floatmap,newfloatmap)
+	"""
 
 
 def setup():
@@ -281,7 +361,7 @@ def setup():
 	screen.blit(smap,(511,0))    	
 	screen.blit(smap,(0,511))    	
 	screen.blit(smap,(511,511))    	
-	pygame.image.save(smap, "omap2.png")
+	pygame.image.save(smap, "original_map.png")
 	
 	old_pos = pygame.mouse.get_pos()
 	while running:
@@ -297,6 +377,7 @@ def setup():
 		pygame.display.update()
 		tick()
 		clock.tick(50)
+		button_down = True
 		if(button_down):
 			pos = pygame.mouse.get_pos()
 			rel_x = pos[0] - old_pos[0]
@@ -304,30 +385,30 @@ def setup():
 			offset_x += rel_x
 			offset_y += rel_y
 			old_pos = pos;
-		"""
-		print("generating map")
-		floatmap = generate_map(1.0)
-		print("generating done")
-		smap = floatmap2surface(floatmap)
-		screen.blit(smap,(0,0))    	
-		screen.blit(smap,(511,0))    	
-		screen.blit(smap,(0,511))    	
-		screen.blit(smap,(511,511))    	
-		"""
-		#print("generating map")
+			"""
+			print("generating map")
+			floatmap = generate_map(1.0)
+			print("generating done")
+			smap = floatmap2surface(floatmap)
+			screen.blit(smap,(0,0))    	
+			screen.blit(smap,(511,0))    	
+			screen.blit(smap,(0,511))    	
+			screen.blit(smap,(511,511))    	
+			"""
+			#print("generating map")
 			#map = generate_map(1.0)
 			#render_map(map,offset_x,offset_y,255)
 			#print("generating done")
-		print("eroding map")
-		for i in range(50):
-			newfloatmap = createfloatmap(-1.0)
-			erode(random.randint(0,pow(2,iterations)-1),random.randint(0,pow(2,iterations)-1),floatmap,newfloatmap)
-			updatefloatmap(floatmap,newfloatmap,-1.0)
+			print("eroding map")
+			for i in range(3000):
+				#newfloatmap = createfloatmap(-1.0)
+				erode(random.randint(0,pow(2,iterations)-1),random.randint(0,pow(2,iterations)-1),random.uniform(0.01,0.02),random.uniform(0.1,0.5),floatmap)
+				#updatefloatmap(floatmap,newfloatmap,-1.0)
 			#erode(pos[0],pos[1],floatmap,newfloatmap)
-		print("eroding done")
-		smap = floatmap2surface(floatmap)
-		pygame.image.save(smap, "dmap.png")
-		screen.blit(smap,(0,0))    	
+			print("eroding done")
+			smap = floatmap2surface(floatmap)
+			pygame.image.save(smap, "erosion_map.png")
+			screen.blit(smap,(0,0))    	
 			#floatmap = newfloatmap
 			#render_map(map,offset_x,offset_y,255)
 			#h = pos2height(pos[0],pos[1],map)
